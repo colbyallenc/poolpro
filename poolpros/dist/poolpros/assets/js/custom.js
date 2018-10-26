d474eaaa0fc79acef7bfc61a0b877f2ab1ec02ed
colbyallenc <colbyallenc@gmail.com>
2018-10-26 14:58:48 -0400

    build

---
+++ poolpros/dist/poolpros/assets/js/custom.js
@@ -0,0 +1,33 @@
+
+/*jQuery('.filter-title').mousedown(function() {  
+	var clicks = jQuery(this).data('clicks');
+	jQuery(this).data("clicks", !clicks);  
+	if (clicks) {     
+		jQuery(".filter-opts").removeClass('open');
+		jQuery(this).css('border-bottom','1px solid #ddd');
+	} 
+		else {   
+			jQuery(".filter-opts").addClass('open'); 
+			jQuery(this).css('border-bottom','0px');   
+		}  
+		
+	});
+
+
+
+jQuery('.toggle').mousedown(function() {
+	jQuery(".navbar-nav").addClass('open');
+});
+jQuery('.close-menu').mousedown(function() {
+	jQuery(".navbar-nav").removeClass('open');
+});*/
+
+/*jQuery('.send-email').click(function() {
+	jQuery(".email-modal").addClass('open');
+	jQuery("body").css('overflow','hidden');
+});
+
+jQuery('.close-modal').click(function() {
+	jQuery(".email-modal").removeClass('open');
+	jQuery("body").css('overflow','inherit');
+});*/
